6c266be67eebb88a012f1ae17007611405d948ad
Ilham <ilhamsiddik30@gmail.com>
2026-05-09 10:22:54 +0700

    fix apache mpm finallll yoyoyo

--- auth/login.php
+++ auth/login.php
@@ -1,10 +1,16 @@
 <?php
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 
 include __DIR__ . '/../config/database.php';
 
-$data = json_decode(file_get_contents("php://input"), true);
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit(0);
+}
+
+$data = json_decode(file_get_contents("php://input"), true) ?? [];
 
 $no_hp = trim($data['no_hp'] ?? '');
 
@@ -16,11 +22,11 @@ if (!$no_hp) {
     exit;
 }
 
-// normalize nomor HP
-$no_hp = str_replace("+62", "0", $no_hp);
+/* normalize nomor */
+$no_hp = str_replace(["+62", " "], ["0", ""], $no_hp);
 $no_hp = mysqli_real_escape_string($conn, $no_hp);
 
-// query aman
+/* query */
 $sql = "SELECT id, nama, no_hp, role 
         FROM users 
         WHERE TRIM(no_hp) = '$no_hp' 
